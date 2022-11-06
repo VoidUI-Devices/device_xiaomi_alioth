d6cf83f95fd596f96d4fa92681e6a41246504ee3
johnmart19 <johnivan19999@gmail.com>
2022-11-06 06:04:07 +0200

    Alioth: Import Dolby Config
    - From Miui 14.0.7.0 EU
    - Also disabled Volume Leveler

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
 
+# Dolby Config File
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Miui Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
