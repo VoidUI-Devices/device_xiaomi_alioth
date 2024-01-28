f053d0c208cfe9a83a3e289d33abdf070d3ef82e
johnmart19 <johnivan19999@gmail.com>
2024-01-28 23:34:19 +0200

    Alioth: Update fingerprint to Miui 14.0.8.0 Global

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -19,7 +19,7 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/aliothin:13/TKQ1.220829.002/V14.0.2.0.TKHINXM:user/release-keys",
+    .build_fingerprint = "Mi/aliothin/aliothin:13/TKQ1.220829.002/V14.0.5.0.TKHINXM:user/release-keys",
 
     .nfc = false,
 };
@@ -34,7 +34,7 @@ static const variant_info_t alioth_global_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys",
+    .build_fingerprint = "POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -49,7 +49,7 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "Redmi K40",
     .model = "M2012K11AC",
-    .build_fingerprint = "Redmi/alioth/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHCNXM:user/release-keys",
+    .build_fingerprint = "Redmi/alioth/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHCNXM:user/release-keys",
 
     .nfc = true,
 };
