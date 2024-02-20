70cf3ce8733e508510ca57d46037b51c1a83cb2c
johnmart19 <johnivan19999@gmail.com>
2024-02-20 14:07:15 +0200

    Alioth: Switch to per-device appropriate namings

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -13,8 +13,8 @@ static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
-    .mod_device = "alioth_global",
-    .name = "alioth_global",
+    .mod_device = "aliothin",
+    .name = "aliothin",
     .brand = "Mi",
     .device = "aliothin",
     .marketname = "Mi 11X",
@@ -43,8 +43,8 @@ static const variant_info_t alioth_info = {
     .hwc_value = "",
     .sku_value = "",
 
-    .mod_device = "alioth_global",
-    .name = "alioth_global",
+    .mod_device = "alioth",
+    .name = "alioth",
     .brand = "Redmi",
     .device = "alioth",
     .marketname = "Redmi K40",
